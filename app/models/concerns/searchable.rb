53c4e1f5ddd9cda478774e668d2df3e0e6d520b4
BriocheBerlin <brigitte.markmann@asquera.de>
2017-03-17 15:28:44 +0100

    Remove stemming for topic_list, change city and languages to lists.

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -2,7 +2,7 @@ module Searchable
   extend ActiveSupport::Concern
 
   included do
-    translates :bio, :main_topic, fallbacks_for_empty_translations: true
+     translates :bio, :main_topic, fallbacks_for_empty_translations: true
 
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
@@ -10,59 +10,37 @@ module Searchable
 
     index_name [Rails.application.engine_name, Rails.env].join('_')
 
-    def self.search(query)
-      __elasticsearch__.search(
-        {
-          query: {
-            multi_match: {
-              query: query,
-              fields: [
-                'firstname',
-                'lastname',
-                'fullname',
-                'twitter',
-                'topic_list',
-                'main_topic_en',
-                'main_topic_de',
-                'languages',
-                'city',
-                'country',
-                'bio_de',
-                'bio_en'
-              ],
-              tie_breaker: 0.3,
-              fuzziness: 'AUTO'
-            }
-          },
-          aggs: {
-            lang: {
-              terms: {
-                field: "languages"
-              }
-            },
-            city: {
-              terms: {
-                field: "city"
-              }
-            }
-          }
-        })
-    end
-
     def as_indexed_json(options={})
-      self.as_json(
+      as_json(
+        # change city to cities (list)
+        {
         only: [:firstname, :lastname, :twitter, :languages, :city, :bio],
-          methods: [:fullname, :topic_list, :bio_en, :bio_de, :main_topic_en, :main_topic_de],
+          methods: [:fullname, :topic_list, :bio_en, :bio_de, :main_topic_en, :main_topic_de, :cities, :split_languages],
           include: {
             medialinks: { only: [:title, :description] }
           }
-      )
+        }
+       )
     end
 
     super_special_settings = {
       index: {
         number_of_shards: 1,
         analysis: {
+          filter: {
+            english_stop: {
+              type:       'stop',
+              stopwords:  '_english_' 
+            },
+            english_possessive_stemmer: {
+              type:       'stemmer',
+              language:   'possessive_english'
+            },
+            german_stop: {
+              type:       'stop',
+              stopwords:  '_german_' 
+            }
+          },
           char_filter: {
             strip_twitter: {
               type: 'pattern_replace',
@@ -81,6 +59,28 @@ module Searchable
               type: 'custom',
               tokenizer: 'keyword', # maren: wie soll das matchen? keyword, nicht keyword? evtl booster nutzen
               filter: ['lowercase']
+            },
+            # elisions????
+            cities_analyzer: {
+              type: 'custom',
+              tokenizer: 'keyword',
+              filter: ['lowercase']
+            },
+            english_without_stemming: {
+              tokenizer:  'standard',
+              filter: [
+                'english_possessive_stemmer',
+                'lowercase',
+                'english_stop'
+              ]
+            },
+            german_without_stemming: {
+              tokenizer:  'standard',
+              filter: [
+                'lowercase',
+                'german_stop',
+                'german_normalization'
+              ]
             }
           }
         }
@@ -89,19 +89,17 @@ module Searchable
 
     settings super_special_settings do
       mappings dynamic: 'false' do
-        indexes :firstname,  type: 'string', analyzer: 'german'
-        indexes :lastname,   type: 'string', analyzer: 'german'
-        indexes :fullname,   type: 'string', analyzer: 'german'
+        indexes :fullname,   type: 'string', analyzer: 'standard'
         indexes :twitter,    type: 'string', analyzer: 'twitter_analyzer'
+        indexes :main_topic_de, type: 'string', analyzer: 'german_without_stemming'
+        indexes :main_topic_en, type: 'string', analyzer: 'english_without_stemming'
         indexes :topic_list, type: 'string', analyzer: 'topic_list_analyzer'
-        indexes :main_topic_de, type: 'string', analyzer: 'german'
-        indexes :main_topic_en, type: 'string', analyzer: 'english'
-        indexes :languages,  type: 'string', analyzer: 'standard' # array? german & english drop down!!!
-        indexes :city,       type: 'string', analyzer: 'standard', 'norms': { 'enabled': false }
-        indexes :country,    type: 'string', analyzer: 'standard' # iso standard
-        indexes :website,    type: 'string', analyzer: 'standard'
         indexes :bio_de,     type: 'string', analyzer: 'german'
         indexes :bio_en,     type: 'string', analyzer: 'english'
+        indexes :split_languages,   type: 'string', analyzer: 'standard', 'norms': { 'enabled': false }
+        indexes :cities, fields: { unmod: { type:  'string', analyzer: 'cities_analyzer' }, standard: { type:  'string', analyzer: 'standard'} }
+        indexes :country,    type: 'string', analyzer: 'standard' # iso standard
+        indexes :website,    type: 'string', analyzer: 'standard'
         indexes :medialinks, type: 'nested' do
           indexes :title
           indexes :description
